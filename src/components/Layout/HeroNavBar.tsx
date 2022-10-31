2cf955cc7d52e60541989fb1b863d25ee400fed9
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-10-31 21:29:41 +0900

    ぷｓｈ

--- src/components/Layout/HeroNavBar.tsx
+++ src/components/Layout/HeroNavBar.tsx
@@ -11,12 +11,12 @@ type Props = {
 export const HeroNavBar: NextPage<Props> = ({ bright }) => {
   return (
     <ul
-      className={`flex items-center justify-end gap-10 opacity-40 pr-10 ${
+      className={`flex items-center justify-end gap-10 pr-10 opacity-40 ${
         bright === 'light' ? 'bg-gray-50' : 'bg-gray-200'
       }`}
     >
       {headerLink.map((content) => (
-        <li>
+        <li key={content.label}>
           <Link href={content.link}>
             <a className="cursor-pointer">
               <BaseText content="middle" color="dark">
